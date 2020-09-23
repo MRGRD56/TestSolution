991067e443e3206e88c2bfacd0da624a6e63047a
Голиков Кирилл Владимирович <golikov.kv0467@okeit.edu>
2020-09-23 15:20:02 +0500

    First

---
+++ TestSolution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestSolution
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var strings = new List<string>()
+            {
+                "somesrf один два",
+                "123adsrf  fdefd",
+                "helloi wordl1"
+            };
+            strings
+                .Where(x => x.Contains("1"))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
